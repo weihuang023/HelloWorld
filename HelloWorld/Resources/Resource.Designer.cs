454b75c2999cfee9039c458a0a3e53dd3d964bfa
Wei Huang <Whuang@1800flowers.com>
2017-03-03 22:00:00 -0500

    Init Commit

--- HelloWorld/Resources/Resource.Designer.cs
+++ HelloWorld/Resources/Resource.Designer.cs
@@ -57,6 +57,34 @@ namespace HelloWorld
 			}
 		}
 		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f050002
+			public const int button1 = 2131034114;
+			
+			// aapt resource value: 0x7f050003
+			public const int button2 = 2131034115;
+			
+			// aapt resource value: 0x7f050004
+			public const int button3 = 2131034116;
+			
+			// aapt resource value: 0x7f050001
+			public const int editText1 = 2131034113;
+			
+			// aapt resource value: 0x7f050000
+			public const int textView1 = 2131034112;
+			
+			static Id()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Id()
+			{
+			}
+		}
+		
 		public partial class Layout
 		{
 			
