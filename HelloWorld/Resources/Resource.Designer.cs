8480ffd516930087bae6f8e9ae802778b8803cf6
Wei Huang <Whuang@1800flowers.com>
2017-03-04 19:03:31 -0500

    Added View

--- HelloWorld/Resources/Resource.Designer.cs
+++ HelloWorld/Resources/Resource.Designer.cs
@@ -60,20 +60,35 @@ namespace HelloWorld
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050002
-			public const int button1 = 2131034114;
+			// aapt resource value: 0x7f050006
+			public const int imageButton1 = 2131034118;
 			
-			// aapt resource value: 0x7f050003
-			public const int button2 = 2131034115;
+			// aapt resource value: 0x7f050007
+			public const int imageButton2 = 2131034119;
+			
+			// aapt resource value: 0x7f050008
+			public const int imageButton3 = 2131034120;
+			
+			// aapt resource value: 0x7f050002
+			public const int imageView1 = 2131034114;
 			
 			// aapt resource value: 0x7f050004
-			public const int button3 = 2131034116;
-			
-			// aapt resource value: 0x7f050001
-			public const int editText1 = 2131034113;
+			public const int imageView2 = 2131034116;
 			
 			// aapt resource value: 0x7f050000
-			public const int textView1 = 2131034112;
+			public const int linearLayout1 = 2131034112;
+			
+			// aapt resource value: 0x7f050001
+			public const int linearLayout2 = 2131034113;
+			
+			// aapt resource value: 0x7f050005
+			public const int linearLayout3 = 2131034117;
+			
+			// aapt resource value: 0x7f050003
+			public const int textView1 = 2131034115;
+			
+			// aapt resource value: 0x7f050009
+			public const int textView2 = 2131034121;
 			
 			static Id()
 			{
